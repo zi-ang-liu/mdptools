9de911bdeb9fd9d78280a7b6ffc2ba172ca6bf85
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2025-09-06 09:52:54 +0900

    a

--- mdptools/mdp.py
+++ mdptools/mdp.py
@@ -2,12 +2,23 @@ import numpy as np
 
 
 class MDP:
-    def __init__(self, P: np.ndarray, R: np.ndarray, gamma: float = 0.995):
+    def __init__(
+        self,
+        S: np.ndarray,
+        A: np.ndarray,
+        P: np.ndarray,
+        R: np.ndarray,
+        gamma: float = 0.995,
+    ):
         """
         Markov Decision Process (MDP) class.
 
         Parameters
         ----------
+        S : np.ndarray
+            State space of shape (n_state, state_dim).
+        A : np.ndarray
+            Action space of shape (n_action, action_dim).
         P : np.ndarray
             Transition probability matrix of shape (n_state, n_action, n_state).
         R : np.ndarray
@@ -15,6 +26,8 @@ class MDP:
         gamma : float, optional
             Discount factor, by default 0.995.
         """
+        self.S = S
+        self.A = A
         self.P = P
         self.R = R
         self.gamma = gamma
